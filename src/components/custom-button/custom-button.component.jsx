bb5b9d9346391f55679d4493dcef20a287f2585d
Andreas Börjesson <borjesson.andreas@gmail.com>
2020-01-10 13:17:13 +0100

    Users created from google auth.

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -3,8 +3,6 @@ import React from 'react';
 import './custom-button.styles.scss';
 
 const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => {
-  console.log('google? ' + isGoogleSignIn);
-
   return (
     <button
       className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`}
